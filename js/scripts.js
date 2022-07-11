4dad2638e68a0571731df14bb4285d3907711cb8
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-11 21:45:05 +0100

    bug fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
-/*!
-* Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+const cv_download_btn = document.querySelector('.cv-btn');
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -51,4 +44,9 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
+});
+
+//show alert on cv download button click
+cv_download_btn.addEventListener('click', () => {
+    ohSnap('Succefully created your account');
 });
