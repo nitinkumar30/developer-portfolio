0b90ddc1742182e8fecd530ea1827fe60876c936
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-11 17:24:46 +0100

    add animations

---
+++ js/animations.js
@@ -0,0 +1,25 @@
+ScrollReveal().reveal('.my-image', {
+    delay: 500,
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    reset: true,
+    useDelay: 'always',
+    viewFactor: 0.5,
+    scale: 0.8,
+    easing: 'ease-in-out',
+    mobile: true,
+});
+
+ScrollReveal().reveal('.main-header-title', {
+    delay: 500,
+    duration: 1000,
+    distance: '100px',
+    origin: 'left',
+    reset: true,
+    useDelay: 'always',
+    viewFactor: 0.5,
+    scale: 0.8,
+    easing: 'ease-in-out',
+    mobile: true,
+});
