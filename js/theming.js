6df9b27cbb1bb521fbb4b8c2732485ae2e70452a
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-12 13:49:32 +0100

    add theme btn

--- js/theming.js
+++ js/theming.js
@@ -1,1 +1,30 @@
 const theme_dropdown = document.getElementById('theme-dropdown');
+
+const themes = [
+    {
+        bs_primary_rgb: '#007bff',
+        bs_bg_opacity: '0.9',
+        btn_primary: '#007bff',
+    },
+    {
+        bs_primary_rgb: '#dc3545',
+        bs_bg_opacity: '0.9',
+        btn_primary: '#dc3545',
+    },
+    {
+        bs_primary_rgb: '#28a745',
+        bs_bg_opacity: '0.9',
+        btn_primary: '#28a745',
+    },
+    {
+        bs_primary_rgb: '#fd7e14',
+        bs_bg_opacity: '0.9',
+        btn_primary: '#fd7e14',
+    },
+    {
+        bs_primary_rgb: '#6c757d',
+        bs_bg_opacity: '0.9',
+        btn_primary: '#6c757d',
+    }
+]
+
