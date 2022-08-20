e6cf33809bb6e0c527f4b371c04789cd4d0ff3cd
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-08-21 00:50:54 +0100

    bug fixes

--- js/animations.js
+++ js/animations.js
@@ -43,7 +43,7 @@ ScrollReveal().reveal('.about-text-1', {
 
 ScrollReveal().reveal('.about-text-2', {
     duration: 1300,
-    origin: "right",
+    origin: "left",
     distance: "300px",
     delay: 300,
     easing: "ease-in-out",
